5d584d07a8a92a786792e7a7ff5df4c8a84c3425
Roland Marchand <roland.marchand@protonmail.com>
2023-11-15 07:24:37 -0500

    Improved code structure, fixed race condition bug with the websocket connections

---
+++ backend/place.go
@@ -0,0 +1,362 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type database struct {
+	client  *redis.Client
+	context context.Context
+}
+
+func getDatabaseConnection() database {
+	password := os.Getenv("REDIS_PASSWORD")
+	username := os.Getenv("REDIS_USERNAME")
+	db, _ := strconv.Atoi(os.Getenv("REDIS_DATABASE"))
+	addr := os.Getenv("REDIS_ADDRESS")
+	if addr == "" {
+		addr = "localhost:6379"
+	}
+
+	ret := database{
+		client: redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: password,
+			Username: username,
+			DB:       db,
+		}),
+		context: context.Background(),
+	}
+	_, err := ret.client.Ping(ret.context).Result()
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func middleware(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusOK)
+		return
+	}
+	c.Next()
+}
+
+var (
+	clients     = make(map[*websocket.Conn]chan pixelData)
+	broadcast   = make(chan pixelData)
+	clientMutex sync.Mutex
+	upgrader    = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+const maxPixelRows = 1024
+const maxPixelColumns = 1024
+const colorDefault = "#FFFFFF"
+
+type pixelData struct {
+	X     string `json:"x"`
+	Y     string `json:"y"`
+	Color string `json:"color"`
+}
+
+const getTimeout time.Duration = 10 * time.Millisecond
+const setTimeout time.Duration = 20 * time.Second
+
+func startSetTimeout(ip *string, db *database) {
+	now, _ := time.Now().MarshalText()
+	db.client.Set(db.context, "set_timeout:"+*ip, now, setTimeout)
+}
+
+func startgetTimeout(ip *string, db *database) {
+	now, _ := time.Now().MarshalText()
+	db.client.Set(db.context, "get_timeout:"+*ip, now, getTimeout)
+}
+
+func approveSetRequest(ip *string, db *database) (bool, time.Duration) {
+	data, err := db.client.Get(db.context, "set_timeout:"+*ip).Result()
+	// Timeouts in Redis are set to expire
+	if err == redis.Nil {
+		return true, 0 // expired
+	}
+	if err != nil {
+		panic(err)
+	}
+	since, _ := time.Parse(time.RFC3339, data)
+	return false, setTimeout - time.Since(since)
+}
+
+func approveGetRequest(ip *string, db *database) (bool, time.Duration) {
+	data, err := db.client.Get(db.context, "get_timeout:"+*ip).Result()
+	// Timeouts in Redis are set to expire
+	if err == redis.Nil {
+		return true, 0 // expired
+	}
+	if err != nil {
+		panic(err)
+	}
+	since, _ := time.Parse(time.RFC3339, data)
+	return false, getTimeout - time.Since(since)
+}
+
+func getPixels(db *database, columns, rows int) [][]string {
+	ret := make([][]string, columns)
+	for x := 0; x < columns; x++ {
+		ret[x] = make([]string, rows)
+		for y := 0; y < rows; y++ {
+			key := strconv.Itoa(x) + "x" + strconv.Itoa(y)
+			color, err := db.client.Get(db.context, key).Result()
+			if err == redis.Nil {
+				ret[x][y] = colorDefault
+			} else if err != nil {
+				panic(err)
+			} else {
+				ret[x][y] = color
+			}
+		}
+	}
+	return ret
+}
+
+func denyPixelPlacement(conn *websocket.Conn, ip *string) error {
+	// Notify the client of the limit
+	seconds := fmt.Sprintf("%.0f", setTimeout.Seconds())
+	msg := "rate limit exceeded of 1 request per " +
+		seconds + " seconds"
+	data, _ := json.Marshal(map[string]string{
+		"Error": msg,
+	})
+	log.Println("Notice: ", ip, string(msg))
+	err := conn.WriteMessage(websocket.TextMessage, data)
+	return err
+}
+
+func allowPixelPlacement(conn *websocket.Conn, db *database, ip *string, pixel pixelData) error {
+	log.Printf("debug: from %s: %v\n", *ip, pixel)
+	key := pixel.X + "x" + pixel.Y
+
+	clientMutex.Lock()
+	startSetTimeout(ip, db)
+	err := db.client.Set(db.context, key, pixel.Color, 0).Err()
+	clientMutex.Unlock()
+	if err != nil {
+		panic(err)
+	}
+
+	broadcast <- pixel
+
+	// Notify the client of the expected wait time
+	data, _ := json.Marshal(map[string]float64{
+		"waitSeconds": setTimeout.Seconds(),
+	})
+	return conn.WriteMessage(websocket.TextMessage, data)
+}
+
+func marshalPayload(conn *websocket.Conn, payload []byte, pixel *pixelData) (success bool, err error) {
+	if err = json.Unmarshal(payload, &pixel); err != nil {
+		log.Println("Warning: invalid payload: ", err)
+		data, _ := json.Marshal(map[string]string{
+			"Error": "Badly formed payload",
+		})
+		success, err = false, conn.WriteMessage(websocket.TextMessage, data)
+		return
+	}
+	success, err = true, nil
+	return
+}
+
+func handleWebSocketConnection(conn *websocket.Conn, db *database, ip *string) {
+	type WebSocketMessage struct {
+		Type    int
+		Payload []byte
+		Err     error
+	}
+	var wg sync.WaitGroup
+	message := make(chan WebSocketMessage)
+	read := func() {
+		defer wg.Done()
+		wg.Add(1)
+		var msg WebSocketMessage
+		msg.Type, msg.Payload, msg.Err = conn.ReadMessage()
+		message <- msg
+	}
+
+	defer conn.Close()
+	defer wg.Wait()
+	defer close(message)
+	defer delete(clients, conn)
+
+	var pixel pixelData
+	broadcastedSignal := clients[conn]
+	go read()
+
+	for {
+		select {
+		case pixel = <-broadcastedSignal:
+			err := conn.WriteJSON(pixel)
+			if err != nil {
+				log.Println("Warning: ", err)
+				return
+			}
+		case msg := <-message:
+			// Applications must break out of the read loop when
+			// this method returns a non-nil error value
+			if msg.Err != nil {
+				log.Println("Warning: ", msg.Err)
+				return
+			}
+			if msg.Type == websocket.CloseMessage {
+				log.Println("Notice: ", ip, ": received closed message")
+				return
+			}
+
+			success, err := marshalPayload(conn, msg.Payload, &pixel)
+			if err != nil {
+				log.Println("Warning: ", msg.Err)
+				return
+			}
+			if !success {
+				read()
+				continue
+			}
+
+			// Enforce the 1 pixel per setTimeout rule
+			if approved, _ := approveSetRequest(ip, db); !approved {
+				err := denyPixelPlacement(conn, ip)
+				if err != nil {
+					log.Println("Warning: ", err)
+					return
+				}
+				go read()
+				continue
+			}
+			err = allowPixelPlacement(conn, db, ip, pixel)
+			if err != nil {
+				log.Println("Warning: ", err)
+				return
+			}
+			go read()
+		}
+	}
+}
+
+func handleBroadcast() {
+	for {
+		pixel := <-broadcast
+		for client, channel := range clients {
+			log.Printf("debug: to %s: %v\n",
+				client.LocalAddr().String(), pixel)
+			channel <- pixel
+		}
+	}
+}
+
+func main() {
+	db := getDatabaseConnection()
+	defer db.client.Close()
+
+	var debugMode bool = os.Getenv("PLACE_RELEASE_BUILD") != "release"
+	if !debugMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+	r.Use(middleware)
+	r.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println("error: ", err)
+			c.JSON(http.StatusInternalServerError,
+				gin.H{"error": err.Error()})
+			return
+		}
+
+		clients[conn] = make(chan pixelData)
+		// using Gin's IP format for consistency
+		ip := c.ClientIP()
+		go handleWebSocketConnection(conn, &db, &ip)
+	})
+	r.GET("/", func(c *gin.Context) {
+		ip := c.ClientIP()
+
+		if approved, _ := approveGetRequest(&ip, &db); !approved {
+			log.Println("Notice: ", ip, ": too many requests")
+			c.AbortWithStatus(http.StatusTooManyRequests)
+			return
+		}
+
+		columns, err := strconv.Atoi(c.Query("columns"))
+		if err != nil {
+			log.Println("Warning: bad request: " + c.Query("columns"))
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		rows, err := strconv.Atoi(c.Query("rows"))
+		if err != nil {
+			log.Println("Warning: bad request: " + c.Query("rows"))
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if columns > maxPixelColumns {
+			log.Println("Notice: ", ip,
+				": request entity too large: ",
+				"request columns ", columns,
+				" is over max allowed of ", maxPixelColumns)
+			c.AbortWithStatus(http.StatusRequestEntityTooLarge)
+			return
+		}
+		if rows > maxPixelRows {
+			log.Println("Notice: ", ip,
+				": request entity too large: ",
+				"request rows ", rows,
+				" is over max allowed of ", maxPixelRows)
+			c.AbortWithStatus(http.StatusRequestEntityTooLarge)
+			return
+		}
+
+		pixels := getPixels(&db, columns, rows)
+		_, timeLeft := approveSetRequest(&ip, &db)
+		data, err := json.Marshal(map[string]interface{}{
+			"pixels":   pixels,
+			"timeLeft": timeLeft.Seconds(),
+		})
+		if err != nil {
+			panic(err)
+		}
+		c.Data(http.StatusOK, "application/json", data)
+		// Forbid GET spam
+		startgetTimeout(&ip, &db)
+	})
+	go handleBroadcast()
+
+	port := os.Getenv("PLACE_PORT")
+	if port == "" {
+		port = "37372"
+	}
+	if !debugMode {
+		r.RunTLS(":"+port, "fullchain.pem", "privkey.pem")
+	} else {
+		r.Run(":" + port)
+	}
+}
